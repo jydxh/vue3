5b8d6b35bc2cb972ef0d768d94791f390bfee045
jydxh <wzxxds@gmail.com>
2023-11-12 19:47:55 -0500

    data response

--- vue3-project/src/views/HomeView.vue
+++ vue3-project/src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<hr />
+		<button @click="next">click me</button>
+		<h3>welcome {{ username }}</h3>
+		<h3>电影名称: {{ movie.title }}</h3>
+		<h3>电影主演: {{ movie.actors.join("/") }}</h3>
+		<h3>上映时间: {{ movie.showingon }}</h3>
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+	import { defineComponent, ref } from "vue";
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+	export default defineComponent({
+		// 当组件创建时 将自动调用该方法 完成数据的基本配置
+		// setup方法return的对象中的数据，在template页面中可以直接使用
+		setup() {
+			let movie = {
+				title: "如果声音不记得",
+				actors: ["亮亮", "泡泡", "鑫哥"],
+				showingon: "2023-11-12",
+			};
+			let username = ref("华哥");
+
+			let next = () => {
+				username.value = "东哥";
+			};
+			return { movie, username, next }; // 这个是个object {movie: movie, username: username}
+		},
+	});
 </script>
