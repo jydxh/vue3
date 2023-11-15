b3aefe4cfb0c9694f26d08d384a79336ccb588c5
jydxh <wzxxds@gmail.com>
2023-11-15 13:02:50 -0500

    finish the first page

---
+++ bmdstudios-mobile-client/src/types/Movie.ts
@@ -0,0 +1,13 @@
+interface Movie {
+  category_id: number;
+  cover: string;
+  description: string;
+  duration: number;
+  id: number;
+  score: string;
+  showingon: string;
+  star_actor: string;
+  title: string;
+  type: string;
+}
+export default Movie;
--- bmdstudios-mobile-client/src/views/home/Index.vue
+++ bmdstudios-mobile-client/src/views/home/Index.vue
@@ -1,52 +1,75 @@
 <template>
 	<div>
 		<!-- 标题栏 -->
-		<van-nav-bar title="百慕大影城">
-			<template #right>
-				<van-popover v-model:show="showPopover" :actions="actions" placement="bottom-end">
-					<template #reference>
-						<van-icon name="apps-o" size="22" />
-					</template>
-				</van-popover>
-			</template>
-		</van-nav-bar>
+		<van-sticky>
+			<van-nav-bar title="百慕大影城">
+				<template #right>
+					<van-popover v-model:show="showPopover" :actions="actions" placement="bottom-end">
+						<template #reference>
+							<van-icon name="apps-o" size="22" />
+						</template>
+					</van-popover>
+				</template>
+			</van-nav-bar>
 
-		<!--appheader advertisements -->
-		<app-header></app-header>
+			<!--appheader advertisements -->
+			<app-header style="background-color: white"></app-header>
 
-		<!-- 顶部导航 -->
-		<div class="nav">
-			<p>北京</p>
-			<span class="arrow"></span>
-			<!-- 标签组 -->
+			<!-- 顶部导航 -->
+			<div class="nav">
+				<p>北京</p>
+				<span class="arrow"></span>
+				<!-- 标签组 -->
 
-			<van-tabs class="tabs" v-model:active="navActive" color="#f03d37">
-				<van-tab title="热映" name="1"></van-tab>
-				<van-tab title="待映" name="2"></van-tab>
-				<van-tab title="经典" name="3"></van-tab>
-			</van-tabs>
+				<van-tabs class="tabs" v-model:active="navActive" color="#f03d37">
+					<van-tab title="热映" name="1"></van-tab>
+					<van-tab title="待映" name="2"></van-tab>
+					<van-tab title="经典" name="3"></van-tab>
+				</van-tabs>
 
-			<!-- 搜索图标 -->
-			<van-icon class="icon" name="search" size="22px" color="#f03d37" style="font-weight: bold" />
-		</div>
-		<van-empty>首页</van-empty>
+				<!-- 搜索图标 -->
+				<van-icon class="icon" name="search" size="22px" color="#f03d37" style="font-weight: bold" />
+			</div>
+		</van-sticky>
+		<!-- 	<van-empty>首页</van-empty> -->
+		<!-- 电影列表 -->
+		<movieItem v-for="item in movies" :key="item.id" :movie="item" class="movieItem"></movieItem>
+		<div style="height: 50px"></div>
 	</div>
 </template>
 
 <script lang="ts" setup>
-	import { ref } from "vue";
+	import { onMounted, ref, reactive } from "vue";
+	import httpApi from "@/http";
+	import Movie from "@/types/Movie";
 	// 顶部标题栏相关
 	const showPopover = ref(false);
 	const actions = [{ text: "首页" }, { text: "热点" }, { text: "新闻" }];
 
 	// 顶部导航栏
 	const navActive = ref("1");
+
+	/* 可以重复调用 */
+	const movies = reactive<Movie[]>([]);
+	onMounted(() => {
+		/* axios 请求数据 */
+		httpApi.movieApi.queryByCategoryId({ cid: 1, page: 1, pagesize: 20 }).then(res => {
+			console.log(res);
+			//将res.data.data.result 中的数组交给template渲染
+			movies.push(...res.data.data.result);
+		});
+	});
 </script>
 <style lang="scss" scoped>
+	.wrapper {
+		position: fixed;
+	}
 	.nav {
 		display: flex;
 		align-items: center;
 		padding: 0px 15px;
+		border-bottom: 1px solid #e2e2e2;
+		background-color: white;
 		> .arrow {
 			display: block;
 			border: 5px solid transparent;
